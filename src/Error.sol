330f505f971a5dfb7a6cccd6edd46b9643b00109
Ugo Arzur <ugo.arzur@fabernovel.ey.com>
2023-09-20 15:00:55 +0200

    experiment on errors

---
+++ src/Error.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.21;
+
+contract Error {
+  error NotAuthorized();
+
+  // Function can be called from another contract
+  function throwError() external pure {
+    require(false, "not authorized");
+  }
+
+  function throwCustomError() external pure {
+    revert NotAuthorized();
+  }
+}
